dd5cc54e976eb8b4f676fcb4240146390de98859
RyotaMono <RyotaMono@users.noreply.github.com>
2024-10-01 17:46:23 +0800

    chore: reset timer

--- assets/js/comingsoon.js
+++ assets/js/comingsoon.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("Sep 12, 2024 00:00:00").getTime();
+    const countDate = new Date("Oct 31, 2024 00:00:00").getTime();
 
     const currentTime = new Date().getTime();
     // console.log(currentTime);
